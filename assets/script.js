b3e76d7841fc6bcc4c0a51864bf42f3562826326
justice <unankajustice@gmail.com>
2023-06-08 16:07:24 +0100

    background change

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,90 @@
 const fruitForm = document.querySelector("#inputSection form")
-console.log(fruitForm)
-
-const fruitNutrition = document.querySelector("#nutritionSection p")
-let cal = 0
-console.log(fruitForm)
 const fruitList = document.querySelector("#fruitSection ul")
+const fruitNutrition = document.querySelector("#nutritionSection p")
+const createForm = document.querySelector("#create-form");
 
-const addFruit = (fruit) => {
-    const li = document.createElement('li')
-    li.textContent = `${fruit.name} of Genus ${fruit.genus}`
-    li.addEventListener("click", (e) => {
-        cal -= fruit.nutritions.calories;
-        if (cal === 0) {
-            fruitNutrition.textContent = "";
-        } else {
-            fruitNutrition.textContent = cal;
-        }
-        e.target.remove()
+let cal = 0
+const fruitCal = {}
+const apiKey = "37099370-66e6515ea1eec39df2884a14d"//key to your api
 
+fruitForm.addEventListener("submit", extractFruit);
+createForm.addEventListener("submit", createNewFruit);
 
-    },
-        { once: true })
-    fruitList.appendChild(li)
-
-    cal += fruit.nutritions.calories
-    fruitNutrition.textContent = cal;
+function extractFruit(e) {
+    e.preventDefault();
+    fetchFruitData(e.target.fruitInput.value);
+    e.target.fruitInput.value = "";
 }
 
-const fetchFruitData = async (fruit) => {
+async function fetchFruitData(fruit) {
     try {
-        const resp = await fetch(`https://fruity-api.onrender.com/api/fruits/${fruit}`)
-        if (resp.ok) {
-            const data = await resp.json()
-            addFruit(data)
+        const repsData = await fetch('https://pixabay.com/api/?key=37099370-66e6515ea1eec39df2884a14d&q=fruits&image_type=photo')
+
+        const respImg = await fetch(`https://pixabay.com/api/?key=37099370-66e6515ea1eec39df2884a14d&q=${fruit}&image_type=photo`);
+        if (repsData.ok && respImg.ok) {
+            const data = await repsData.json();
+            const imgData = await respImg.json();
+            addFruit(data, imgData);
         } else {
-            throw "Error: http status code = " + resp.status
+            throw "something has gone wrong with one of the API requests"
         }
-    } catch (err) {
-        console.log(err)
+    } catch (e) {
+        console.log(e)
     }
 }
 
-const extractFruit = (e) => {
-    e.preventDefault()
-    fetchFruitData(e.target.fruitInput.value)
-    e.target.fruitInput.value = ""
+async function createNewFruit(e) {
+    e.preventDefault();
+    const data = { name: e.target.fruitInput.value };
+
+    const options = {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
 }
-fruitForm.addEventListener("submit", extractFruit)
+
+
+
+const response = await fetch(`https://fruity-api.onrender.com/api/fruits/${fruit}`, options);
+let messageStatus = document.querySelector("#message")
+if (response.status === 201) {
+    e.target.fruitInput.value = ''
+    messageStatus.textContent = "Fruit successfully created."
+    setTimeout(() => {
+        messageStatus.textContent = ""
+    }, 4000)
+} else {
+    e.target.fruitInput.value = ''
+    messageStatus.textContent = "This fruit already exists. Please enter another fruit!"
+    setTimeout(() => {
+        messageStatus.textContent = ""
+    }, 4000)
+}
+
+
+function addFruit(fruit, fruitImg) {
+    const img = document.createElement("img");
+    img.classList.add('fruits');
+    img.alt = fruit.name;
+    img.src = fruitImg.hits[0].previewURL;
+
+    img.addEventListener("click", removeFruit, { once: true });
+    fruitList.appendChild(img);
+
+    fruitCal[fruit.name] = fruit.nutritions.calories;
+
+    cal += fruit.nutritions.calories;
+    fruitNutrition.textContent = "Total Calories: " + cal;
+}
+
+function removeFruit(e) {
+    const fruitName = e.target.alt;
+    cal -= fruitCal[fruitName];
+    fruitNutrition.textContent = "Total Calories: " + cal;
+
+    delete fruitCal[fruitName];
+    e.target.remove();
+}
--- bundle.js
+++ bundle.js
@@ -1,13 +1,54 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const x = require('./script2.js')
-console.log("Hello");
-console.log(x);
+const fruitForm = document.querySelector("#inputSection form")
+console.log(fruitForm)
+
+const fruitNutrition = document.querySelector("#nutritionSection p")
+let cal = 0
+console.log(fruitForm)
+const fruitList = document.querySelector("#fruitSection ul")
+
+const addFruit = (fruit) => {
+    const li = document.createElement('li')
+    li.textContent = `${fruit.name} of Genus ${fruit.genus}`
+    li.addEventListener("click", (e) => {
+        cal -= fruit.nutritions.calories;
+        if (cal === 0) {
+            fruitNutrition.textContent = "";
+        } else {
+            fruitNutrition.textContent = cal;
+        }
+        e.target.remove()
 
 
+    },
+        { once: true })
+    fruitList.appendChild(li)
 
-},{"./script2.js":2}],2:[function(require,module,exports){
-const x = 42;
+    cal += fruit.nutritions.calories
+    fruitNutrition.textContent = cal;
+}
 
-module.exports = x;
+const fetchFruitData = async (fruit) => {
+    try {
+        const resp = await fetch(`https://justicesfruitapi.onrender.com/fruits`)
+        if (resp.ok) {
+            const data = await resp.json()
+            addFruit(data)
+        } else {
+            throw "Error: http status code = " + resp.status
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
+//after the api link it didn't work.
+
+    
+const extractFruit = (e) => {
+    e.preventDefault()
+    fetchFruitData(e.target.fruitInput.value)
+    e.target.fruitInput.value = ""
+}
+fruitForm.addEventListener("submit", extractFruit)
 
 },{}]},{},[1]);
